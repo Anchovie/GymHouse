2fcce09ea3b2540a89d3105d256aca96fcf209d9
Panajis Rantala <panajis.rantala@gmail.com>
2016-11-15 21:58:16 +0200

    user and level mappings for calendar

--- schedule/views.py
+++ schedule/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from mainpage.models import Event
 from mainpage.models import Class
+from mainpage.models import *
 import json
 from django.core import serializers
 
@@ -18,11 +19,19 @@ def calendar_view(request):
     classes = Class.objects.all().order_by('begin_date')
     classes = serializers.serialize('json', classes)
 
+    users = {}
+    user_objects = Profile.objects.all()
+    for user in user_objects :
+        users[user.pk]=(str(user.first_name)+" "+str(user.last_name))
+
+    levels = {1:'Beginner',2:'Intermediate',3:'Advanced',4:'Expert'}
+
+
     context = {'user': request.user, 
             'logged_in': request.user.is_authenticated,
             'events': events,
-            'classes': classes}
+            'classes': classes,
+            'users': users,
+            'levels':levels}
 
     return render(request, 'schedule/schedule_template.html', context);
-
-# Create your views here.
