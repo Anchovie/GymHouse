1d3fcef7099ef1a3e60c537ec7a649067715f482
Biraj Paul <biraj.paul@hotmail.com>
2016-12-24 16:46:36 +0200

    Added trainer cards to the trainers template (TBD: Other info for the trainers)

--- services/views.py
+++ services/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+from mainpage.models import Profile
+from django.template.defaulttags import register
 
 
 """
@@ -18,7 +19,16 @@ def services_view(request):
 
 @login_required
 def trainers_view(request):
+
+    userProfiles=Profile.objects.filter(status='TRN')
+    trainer_lvl = {"1": "Beginner", "2": "Intermediate", "3": "Advanced", "4": "Expert"}
     context = {'user': request.user,
-			'logged_in': request.user.is_authenticated}
+			'logged_in': request.user.is_authenticated,
+			'trainers': userProfiles,
+			'trainer_lvl': trainer_lvl}
 
     return render(request, 'services/trainers_template.html', context);
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
